26bb95b2cc2116bb5d5a7d4bf7060ca4ce846d79
LogolsLLC <30915974+LogolsLLC@users.noreply.github.com>
2018-02-11 07:46:05 -0500

    web api exercise

---
+++ Exercises/webapi/ZombieReport.WebAPI/PersonStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace ZombieReport.WebAPI
+{
+    using MySql.Data.MySqlClient;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text;
+
+    public class PersonStatus
+    {
+        public int PersonId { get; set; }
+        public int PersonStatusId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Exercises/webapi/ZombieReport.WebAPI/Repository.cs
@@ -0,0 +1,20 @@
+﻿namespace ZombieReport.WebAPI
+{
+    using MySql.Data.MySqlClient;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text;
+
+    public class Repository
+    {
+        private string connectionString = @"server=localhost;port=3306;database=Assessment;user=[user];password=[pass]";
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new MySqlConnection(connectionString);
+            }
+        }
+    }
+}
