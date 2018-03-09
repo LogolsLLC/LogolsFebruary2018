fd82491f9e99cf641e6c9e8e19db9b9ecde5ffa8
LogolsLLC <30915974+LogolsLLC@users.noreply.github.com>
2018-03-09 17:10:48 -0500

    Angular

---
+++ Exercises/Angular/ZombieReport/src/app/addStatus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { PersonStatus } from './entities/PersonStatus'
+
+@Component({
+  selector: 'addStatus',
+  templateUrl: './addStatus.component.html',
+  styleUrls: ['./addStatus.component.css']
+})
+export class AddStatusComponent {
+
+}
